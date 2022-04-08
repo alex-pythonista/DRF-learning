c4298cec7fa45190b9e320d7df0b7e46ddc642dc
alex-pythonista <indrajit.alex@gmail.com>
2022-04-08 21:46:54 +0600

    1hr done

---
+++ backend/api/views.py
@@ -0,0 +1,15 @@
+from pyexpat import model
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from django.forms.models import model_to_dict
+
+from products.models import *
+# Create your views here.
+
+def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title'])
+    return JsonResponse(data)
