70666f079041e0039b0349505888a7e52a74ac51
alex-pythonista <indrajit.alex@gmail.com>
2022-04-10 21:04:48 +0600

    for today

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,31 @@
-from pyexpat import model
-from django.shortcuts import render
-from django.http import JsonResponse
-import json
 from django.forms.models import model_to_dict
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.http import JsonResponse
 from products.models import *
+from products.serializers import ProductSerializer
+
 # Create your views here.
 
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title'])
-    return JsonResponse(data)
+    serializer = ProductSerializer(data=request.data)
+
+    if serializer.is_valid():
+        instance = serializer.save()
+        print(instance)
+        # data = serializer.data
+        return Response(serializer.data)
+
+
+
+# @api_view(['GET'])
+# def api_home(request, *args, **kwargs):
+#     instance = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if instance:
+#         # data = model_to_dict(model_data, fields=['id', 'title', 'sale_price'])
+#         data = ProductSerializer(instance).data
+#     return Response(data)
